75be3bdeec76e67d7fc43bdb4b53c623bc2d5b7f
ahelyikemeny <kancsalmate27@gmail.com>
2020-11-27 16:11:59 +0100

    Das Hund is grün.

--- core/src/BallGameFolder/WallActor.java
+++ core/src/BallGameFolder/WallActor.java
@@ -1,17 +1,23 @@
 package BallGameFolder;
 
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
+import hu.csanyzeg.master.MyBaseClasses.Box2dWorld.Box2DWorldHelper;
+import hu.csanyzeg.master.MyBaseClasses.Box2dWorld.MyFixtureDef;
+import hu.csanyzeg.master.MyBaseClasses.Box2dWorld.ShapeType;
 import hu.csanyzeg.master.MyBaseClasses.Game.MyGame;
 import hu.csanyzeg.master.MyBaseClasses.Scene2D.OneSpriteStaticActor;
 
 public class WallActor extends OneSpriteStaticActor {
     BallMyGameStage1 firstStage;
-    public WallActor(MyGame game, String hash, BallMyGameStage1 firstStage) {
-        super(game, "badlogic.jpg");
+    public WallActor(MyGame game, World world, BallMyGameStage1 firstStage, float x, float y) {
+        super(game, "box2dhelper/badlogic.jpg");
         this.firstStage = firstStage;
 this.setSize(10, 100);
-
+        MyFixtureDef myFixtureDef = new MyFixtureDef();
+        setActorWorldHelper(new Box2DWorldHelper(world, this, ShapeType., myFixtureDef, BodyDef.BodyType.StaticBody));
 
     }
 
