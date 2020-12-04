052ff1c21a154a59a8d05d46a33f2f8b6e445d1e
Milaan1337 <71323999+Milaan1337@users.noreply.github.com>
2020-12-04 15:21:46 +0100

    xd

--- core/src/BallGameFolder/KozepesStage.java
+++ core/src/BallGameFolder/KozepesStage.java
@@ -8,7 +8,6 @@ import hu.csanyzeg.master.MyBaseClasses.Box2dWorld.WorldBodyEditorLoader;
 import hu.csanyzeg.master.MyBaseClasses.Game.MyGame;
 
 public class KozepesStage extends Box2dStage {
-    WallActor wallActor;
     public KozepesStage(MyGame game) {
         super(new ExtendViewport(150,90), game);
         addActor(new BallActor(game, world, 70,40));
