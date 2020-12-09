88f218efea9d694741eabc79a0e8aea464fbe799
Milaan1337 <71323999+Milaan1337@users.noreply.github.com>
2020-12-09 17:04:44 +0100

    labelStyle

--- core/src/BallGameFolder/labelStyle.java
+++ core/src/BallGameFolder/labelStyle.java
@@ -6,8 +6,8 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label;
 
 import hu.csanyzeg.master.MyBaseClasses.Game.MyGame;
 
-public class LabelStyle extends Label.LabelStyle {
-    public LabelStyle(MyGame game) {
+public class labelStyle extends Label.LabelStyle {
+    public labelStyle(MyGame game) {
         super(game.getMyAssetManager().getFont("alegreyaregular.otf"), Color.WHITE);
     }
 }
