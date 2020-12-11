378e52154b1079ecc24ef8ada08e78710292e981
KeLorand <lorand996@gmail.com>
2020-12-11 15:04:07 +0100

    Interkontinentális Ballisztikus Rakéta elindítva

--- core/src/BallGameFolder/NehezStage.java
+++ core/src/BallGameFolder/NehezStage.java
@@ -15,23 +15,23 @@ public class NehezStage extends BaseGameStage {
     @Override
     public void buildLevel() {
         //fent
-        for(int i = 0; i<=110; i = i + 10){
+        for (int i = 0; i <= 110; i = i + 10) {
             addActor(new GlobalWallActor(game, world, i, 115, 10, 5));
         }
 
 
         //bal
-        for(int i = 5; i<=113; i = i + 3){
+        for (int i = 5; i <= 113; i = i + 3) {
             addActor(new GlobalWallActor(game, world, 0, i, 6, 3));
 
             //jobb
         }
-        for(int i = 5; i<=120; i = i + 3){
+        for (int i = 5; i <= 120; i = i + 3) {
             addActor(new GlobalWallActor(game, world, 154, i, 6, 3));
         }
 
         //lentbal
-        for(int i = 0; i<=10; i = i + 10){
+        for (int i = 0; i <= 10; i = i + 10) {
             addActor(new GlobalWallActor(game, world, i, 0, 10, 5));
         }
 
@@ -68,7 +68,29 @@ public class NehezStage extends BaseGameStage {
 
 
         SensorActor sensorActor;
-        addActor(sensorActor = new SensorActor(game,world,10,0, 60, 3));
+        SensorActor1 sensorActor1;
+
+        addActor(sensorActor = new SensorActor(game, world, 20, 0, 50, 3));
+        addActor(sensorActor1 = new SensorActor1(game, world, 120, 115, 35, 6));
+        sensorActor1.setVisible(false);
+
+        getHelper(sensorActor1).addContactListener(new MyContactListener() {
+            @Override
+            public void beginContact(Contact contact, Box2DWorldHelper myHelper, Box2DWorldHelper otherHelper) {
+                if (otherHelper.getActor() instanceof BallActor){
+                    otherHelper.getActor().setPosition(70,40);
+
+
+                    otherHelper.invoke(new Runnable() {
+                        @Override
+                        public void run() {
+                            otherHelper.getBody().setLinearVelocity(0,0);
+                            otherHelper.getBody().setAngularVelocity(0);
+                        }
+                    });
+                }
+            }
+        });
 
         getHelper(sensorActor).addContactListener(new MyContactListener() {
             @Override
