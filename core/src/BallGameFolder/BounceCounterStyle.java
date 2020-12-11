237f2629ed213a9f7a6f9d3c8e6f5f807777e26c
Milaan1337 <71323999+Milaan1337@users.noreply.github.com>
2020-12-11 15:31:39 +0100

    big update

---
+++ core/src/BallGameFolder/BounceCounterStyle.java
@@ -0,0 +1,13 @@
+package BallGameFolder;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+
+import hu.csanyzeg.master.MyBaseClasses.Game.MyGame;
+
+public class BounceCounterStyle extends Label.LabelStyle {
+    public BounceCounterStyle(MyGame game) {
+        super(game.getMyAssetManager().getFont("alegreyaregular.otf"), Color.WHITE);
+    }
+}
