9e1890e1d8a8b79eb6d4100ba979c6e535c8bbc1
DaW33D <zsebokdavidf@gmail.com>
2020-12-11 17:04:32 +0100

    stage selector backbutton, Help stage

--- core/src/BallGameFolder/HelpStage.java
+++ core/src/BallGameFolder/HelpStage.java
@@ -1,5 +1,7 @@
 package BallGameFolder;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 
 import hu.csanyzeg.master.MyBaseClasses.Game.MyGame;
@@ -10,11 +12,19 @@ import hu.csanyzeg.master.MyBaseClasses.UI.MyLabel;
 public class HelpStage extends MyStage {
  BackButton backButton;
     public  HelpStage(MyGame game){
-        super(new ExtendViewport(160,90), game);
+        super(new ExtendViewport(160,120), game);
+        setCameraResetToCenterOfScreen();
+        Label.LabelStyle labelStyle = new Label.LabelStyle();
+        labelStyle.font = game.getMyAssetManager().getFont("font.ttf");
+        labelStyle.fontColor = Color.BLACK;
         addBackButtonScreenBackByStackPopListener();
         addActor(new InGameBackgroundActor(game));
         backButton = new BackButton(game, 140,0);
         addActor(backButton);
+        MyLabel label = new MyLabel(game, "Info", labelStyle);
+        label.setFontScale(0.2f);
+        label.setPosition(20,70);
+        addActor(label);
 
 
 
